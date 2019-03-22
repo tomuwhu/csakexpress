e75fefae92ad2c20da11458ea274678889d166c3
Tamás Németh <tomuwhu@gmail.com>
2019-03-22 13:18:27 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const Datastore = require('nedb')
+const db = new Datastore({
+        filename: 'mydb.db',
+        autoload: true
+      })
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get(
+    '/',
+    (req, res) => {
+        res.send('Hello World!')
+    }
+)
+
+app.get(
+    '/cica/:id',
+    (req,res) => {
+        db.insert({
+            param: req.params.id
+        })
+        res.send('Rögzítve: ' + req.params.id)
+    }
+)
 
 app.listen(3000)
